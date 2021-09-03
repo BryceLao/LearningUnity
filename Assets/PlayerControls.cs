af09e8ac11c9673adda19df877388cce1afbb32b
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-03 14:36:33 +0800

    Lesson 6 Lesson 7 Lesson 8 Lesson 9 Lesson 10

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public Rigidbody2D rb;                                      
+    private void Update() {      
+        if (Input.GetKey(KeyCode.A)) {
+          rb.velocity= new Vector2(-5, rb.velocity.y);
+          transform.localScale = new Vector2(-1,1);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.velocity= new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1,1);
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.velocity= new Vector2(rb.velocity.x, 8f);
+            
+        }
+    }
+}
+
+
+
+    
+
+
+    
+
