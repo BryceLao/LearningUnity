bb0ae6eadbf2297f2371ee7b33238d788a5fe1d4
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-04 20:22:50 +0800

    Lesson 14

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -4,15 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
-    public Animator anim;
-    public Rigidbody2D rb;
-    private void Update() {      
-        if (Input.GetKey(KeyCode.A)) {
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Animator anim;
+        
+    private void Start(){
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+    }
+    private void Update() {    
+        
+        float HDirection=Input.GetAxis("Horizontal");
+
+        if (HDirection < 0 ) {
           rb.velocity= new Vector2(-5, rb.velocity.y);
           transform.localScale = new Vector2(-1,1);
-            anim.SetBool("running", true);
+        anim.SetBool("running", true);
         }
-        else if (Input.GetKey(KeyCode.D)) {
+        else if (HDirection > 0) {
             rb.velocity= new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1,1);
             anim.SetBool("running", true);
