8086c009f6d93ff2919d91851f17c913aad4dcbd
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-05 12:48:33 +0800

    Lesson 15,16

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,36 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
     private Rigidbody2D rb;
     [SerializeField]
     private Animator anim;
-        
+    private enum State {idle,running, jumping, falling}     
+    private State state= State.idle;
+    private Collider2D coll;
+    [SerializeField]
+    private LayerMask ground;
     private void Start(){
         rb=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
+        coll=GetComponent<Collider2D>();
     }
     private void Update() {    
         
         float HDirection=Input.GetAxis("Horizontal");
 
-        if (HDirection < 0 ) {
+        if(HDirection < 0 ) {
           rb.velocity= new Vector2(-5, rb.velocity.y);
           transform.localScale = new Vector2(-1,1);
-        anim.SetBool("running", true);
         }
-        else if (HDirection > 0) {
+        else if(HDirection > 0) {
             rb.velocity= new Vector2(5, rb.velocity.y);
-            transform.localScale = new Vector2(1,1);
-            anim.SetBool("running", true);
+            transform.localScale =  new Vector2(1,1);
         }
         else{
-            anim.SetBool("running", false);
+
         }
-       if (Input.GetKey(KeyCode.Space)) {
+       if(Input.GetButtonDown("Jump") &&  coll.IsTouchingLayers(ground)) {
             rb.velocity= new Vector2(rb.velocity.x, 10f);   
+            state=State.jumping;
+        }
+       VelocityState();
+       anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityState(){
+        if(state== State.jumping){
+            if(rb.velocity.y < 0.1f){
+                state=State.falling;
+            }
+        }
+        else if(state==State.falling){
+            if(coll.IsTouchingLayers(ground)){
+                state=State.idle;
+            }
+        }
+        else if(Math.Abs(rb.velocity.x) > 2f){
+            state=State.running;   
+        }
+        else{
+            state=State.idle;
         }
     }
 }
