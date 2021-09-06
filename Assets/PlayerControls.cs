ab6791e9d960442d3c49f1634c9bb18bd038363e
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-06 08:53:53 +0800

    Lesson 18

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -12,60 +12,64 @@ public class PlayerControls : MonoBehaviour {
     private enum State {idle,running, jumping, falling}     
     private State state= State.idle;
     private Collider2D coll;
-    [SerializeField]
-    private LayerMask ground;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed=5f;
+    [SerializeField] private float jump=10f;
     private void Start(){
         rb=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
-        coll=GetComponent<Collider2D>();
+        coll=GetComponent<Collider2D>();;
     }
     private void Update() {    
         
-        float HDirection=Input.GetAxis("Horizontal");
+        Movement();
+        VelocityState();
+        anim.SetInteger("state", (int)state);
 
-        if(HDirection < 0 ) {
-          rb.velocity= new Vector2(-5, rb.velocity.y);
-          transform.localScale = new Vector2(-1,1);
-        }
-        else if(HDirection > 0) {
-            rb.velocity= new Vector2(5, rb.velocity.y);
-            transform.localScale =  new Vector2(1,1);
-        }
-        else{
-
-        }
-       if(Input.GetButtonDown("Jump") &&  coll.IsTouchingLayers(ground)) {
-            rb.velocity= new Vector2(rb.velocity.x, 10f);   
-            state=State.jumping;
-        }
-       VelocityState();
-       anim.SetInteger("state", (int)state);
     }
 
     private void VelocityState(){
+
         if(state== State.jumping){
             if(rb.velocity.y < 0.1f){
                 state=State.falling;
             }
         }
+
         else if(state==State.falling){
             if(coll.IsTouchingLayers(ground)){
                 state=State.idle;
             }
         }
+
         else if(Math.Abs(rb.velocity.x) > 2f){
             state=State.running;   
         }
+
         else{
             state=State.idle;
         }
+
+    }
+
+    private void Movement(){
+
+        float HDirection=Input.GetAxis("Horizontal");
+
+        if(HDirection < 0 ) {
+            rb.velocity= new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1,1);
+        }
+
+        else if(HDirection > 0) {
+            rb.velocity= new Vector2(speed, rb.velocity.y);
+            transform.localScale =  new Vector2(1,1);
+        }
+
+        if(Input.GetButtonDown("Jump") &&  coll.IsTouchingLayers(ground)) {
+            rb.velocity= new Vector2(rb.velocity.x, jump);   
+            state=State.jumping;
+        }
+
     }
 }
-
-
-
-    
-
-
-    
-
